4511dbc6872473749549c1e236c76cbaf548eb1e
etsu <kacperox355@gmail.com>
2021-12-01 01:13:50 +0100

    Added /kategorie and /marki which displays them

--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -3,16 +3,32 @@ from flask import render_template, session, request, redirect, url_for, flash
 from shop import app, db, bcrypt
 from .forms import RegistrationForm, LoginForm
 from .models import User
-from shop.produkty.models import dodajProdukt
+from shop.produkty.models import dodajProdukt, Marka, Kategoria
 import os
 
-@app.route('/')
+@app.route('/admin')
 def admin():
     if 'email' not in session:
         flash(f'Proszę się zalogować.','danger')
         return redirect(url_for('login'))
     produkty = dodajProdukt.query.all()
     return render_template('admin/index.html', title="Strona administracyjna", produkty=produkty)
+
+@app.route('/marki')
+def marki():
+    if 'email' not in session:
+        flash(f'Proszę się zalogować.','danger')
+        return redirect(url_for('login'))
+    marki = Marka.query.order_by(Marka.id.desc()).all()
+    return render_template('admin/marki.html', title="Marki", marki = marki)
+
+@app.route('/kategorie')
+def kategorie():
+    if 'email' not in session:
+        flash(f'Proszę się zalogować.','danger')
+        return redirect(url_for('login'))
+    kategorie = Kategoria.query.order_by(Kategoria.id.desc()).all()
+    return render_template('admin/marki.html', title="Marki", kategorie = kategorie)
 
 @app.route('/rejestracja', methods=['GET','POST'])
 def rejestruj():
@@ -24,7 +40,7 @@ def rejestruj():
         db.session.add(user)
         db.session.commit()
         flash(f'Dziękujemy za rejestrację, {form.username.data}','success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('admin/rejestracja.html', form=form, title="Rejestracja")
 
 @app.route('/login', methods=['GET','POST'])
