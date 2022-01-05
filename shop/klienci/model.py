dcc2d3a8a2d70c3a4c9639561a4ec2aa25323871
etsu <kacperox355@gmail.com>
2022-01-05 16:23:09 +0100

    Created customer order table

--- shop/klienci/model.py
+++ shop/klienci/model.py
@@ -1,6 +1,7 @@
 from shop import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+import json
 
 @login_manager.user_loader
 def user_loader(user_id):
@@ -22,6 +23,31 @@ class Rejestracja(db.Model, UserMixin):
     data_stworzenia = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Rejestracja %r>' % self.name
+        return '<Rejestracja %r>' % self.dane
+
+class JsonEncodedDict(db.TypeDecorator):
+    impl = db.Text
+
+    def set_value(self, value, dialect):
+        if value is None:
+            return '{}'
+        else:
+            return json.dumps(value)
+    def get_value(self, value, dialect):
+        if value is None:
+            return {}
+        else:
+            return json.loads(value)
+
+class KlientZamowienie(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    faktura = db.Column(db.String(20), unique = True, nullable = False)
+    status = db.Column(db.String(20), default='Oczekujący', nullable = False)
+    klient_id = db.Column(db.Integer, unique = False, nullable = False)
+    data_stworzenia = db.Column(db.DateTime, default=datetime.utcnow, nullable = False)
+    zamowienia = db.Column(JsonEncodedDict)
+
+    def __repr__(self):
+        return '<KlientZamowienie %r>' % self.faktura
 
 db.create_all()
